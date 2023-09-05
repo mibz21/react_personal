e6499fdd4b5b8a6d0712a0f870af4d90cce9cc9f
mibz21 <mibz0083@gmail.com>
2023-09-05 13:44:05 -0500

    added initial experience page

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,6 +1,9 @@
 import NavBar from "../NavBar/NavBar";
 import '../../App.css';
+import "../everything.css";
+import "./Experience.css"
 import {Helmet} from "react-helmet";
+import Footer from "../Footer/Footer";
 export default function Experience() {
     return(
         <main>
@@ -10,6 +13,23 @@ export default function Experience() {
         <fragment>
             <NavBar/>
         </fragment>
+        <div className="homeContainer">
+            <div className="Container">
+                <div className="description_what">
+                    <h1>Packaging Corporation of America</h1>
+                    <h2>Data Science Intern</h2>
+                </div>
+                <div className="description_what">
+                    <h1>Disruption Lab</h1>
+                    <h2>Software Development Engineer</h2>
+                </div>
+                <div className="description_what">
+                    <h1>Sagence Inc</h1>
+                    <h2>Data Engineering Intern</h2>
+                </div>
+            </div>
+        </div>
+        <Footer />
         </main>
         
         
